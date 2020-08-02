480a219dc694a6c2c90af20be7ca114965781371
Marcin Biernat <mb@marcinbiernat.pl>
2020-08-02 17:06:42 +0200

    Parse highlight, note and bookmark

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use nom::{
     bytes::complete::{tag, take_while},
     character::complete::digit1,
     character::complete::space1,
-    combinator::map_res,
+    combinator::{map_res, opt},
     multi::many1,
     sequence::{preceded, separated_pair, terminated, tuple},
 };
@@ -14,14 +14,44 @@ use std::fs;
 struct Clipping {
     title: String,
     author: Option<String>,
+    content: ClippingContent,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum ClippingContent {
+    Highlight(ClippingHighlight),
+    Note(ClippingNote),
+    Bookmark(ClippingBookmark),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ClippingHighlight {
     location: Location,
-    text: String,
+    highlight: String,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ClippingNote {
+    location: Location,
+    note: String,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ClippingBookmark {
+    location: Location,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 struct Location {
     from: u32,
     to: Option<u32>,
+    kind: LocationKind,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum LocationKind {
+    Page,
+    Location,
 }
 
 fn main() {
@@ -32,23 +62,24 @@ fn main() {
 
     match parsed {
         Ok((_, clippings)) => println!("{:?}", clippings),
-        Err(e) => println!("Errors: {:?}", e),
+        Err(e) => println!("Errors: {:}", e),
     }
 }
 
 fn parse_clipping(input: &str) -> nom::IResult<&str, Clipping> {
     let (input, (title, author)) = parse_title(input)?;
-    // TODO: Note should create a different clipping type
-    let (input, location) = alt((parse_location, parse_note_location))(input)?;
-    let (input, text) = preceded(tag("\r\n"), parse_text)(input)?;
+    let (input, content) = alt((
+        parse_clipping_highlight,
+        parse_clipping_note,
+        parse_clipping_bookmark,
+    ))(input)?;
 
     return Ok((
         input,
         Clipping {
             title: title.into(),
             author: author.map(String::from),
-            location,
-            text: text.into(),
+            content,
         },
     ));
 }
@@ -74,42 +105,75 @@ fn parse_title(input: &str) -> nom::IResult<&str, (&str, Option<&str>)> {
     }
 }
 
+fn parse_clipping_highlight(input: &str) -> nom::IResult<&str, ClippingContent> {
+    let (input, (_, location, _)) =
+        tuple((tag("- Your Highlight "), parse_location, space1))(input)?;
+
+    let (input, _) = terminated(take_while(|c| c != '\r'), tag("\r\n"))(input)?;
+
+    let (input, highlight) = parse_until(tag("\r\n==========\r\n"))(input)?;
+
+    return Ok((
+        input,
+        ClippingContent::Highlight(ClippingHighlight {
+            location,
+            highlight: highlight.into(),
+        }),
+    ));
+}
+
+fn parse_clipping_note(input: &str) -> nom::IResult<&str, ClippingContent> {
+    let (input, (_, location, _)) = tuple((tag("- Your Note "), parse_location, space1))(input)?;
+
+    let (input, _) = terminated(take_while(|c| c != '\r'), tag("\r\n"))(input)?;
+
+    let (input, note) = parse_until(tag("\r\n==========\r\n"))(input)?;
+
+    return Ok((
+        input,
+        ClippingContent::Note(ClippingNote {
+            note: note.into(),
+            location,
+        }),
+    ));
+}
+
+fn parse_clipping_bookmark(input: &str) -> nom::IResult<&str, ClippingContent> {
+    let (input, (_, location, _)) =
+        tuple((tag("- Your Bookmark "), parse_location, space1))(input)?;
+
+    let (input, _) = terminated(take_while(|c| c != '\r'), tag("\r\n"))(input)?;
+    let (input, _) = parse_until(tag("\r\n==========\r\n"))(input)?;
+
+    return Ok((
+        input,
+        ClippingContent::Bookmark(ClippingBookmark { location }),
+    ));
+}
+
 fn parse_location(input: &str) -> nom::IResult<&str, Location> {
-    let (input, _) = tuple((tag("- Your Highlight at location"), space1))(input)?;
-    let (input, (loc_from, loc_to)) = separated_pair(
+    let (input, (kind, _, from, to)) = tuple((
+        alt((tag("at location"), tag("on page"))),
+        tag(" "),
         map_res(digit1, |d| u32::from_str_radix(d, 10)),
-        tag("-"),
-        map_res(digit1, |d| u32::from_str_radix(d, 10)),
-    )(input)?;
-
-    let (input, _) = terminated(take_while(|c| c != '\r'), tag("\r\n"))(input)?;
+        opt(preceded(
+            tag("-"),
+            map_res(digit1, |d| u32::from_str_radix(d, 10)),
+        )),
+    ))(input)?;
 
     Ok((
         input,
         Location {
-            from: loc_from,
-            to: Some(loc_to),
+            kind: match kind {
+                "at location" => LocationKind::Location,
+                "on page" => LocationKind::Page,
+                _ => panic!(format!("Unexpected tag {}", kind)),
+            },
+            from,
+            to,
         },
     ))
-}
-
-fn parse_note_location(input: &str) -> nom::IResult<&str, Location> {
-    let (input, _) = tuple((tag("- Your Note at location"), space1))(input)?;
-    let (input, loc_from) = map_res(digit1, |d| u32::from_str_radix(d, 10))(input)?;
-
-    let (input, _) = terminated(take_while(|c| c != '\r'), tag("\r\n"))(input)?;
-
-    Ok((
-        input,
-        Location {
-            from: loc_from,
-            to: None,
-        },
-    ))
-}
-
-fn parse_text(input: &str) -> nom::IResult<&str, &str> {
-    parse_until(tag("\r\n==========\r\n"))(input)
 }
 
 pub fn parse_until<'a, E: nom::error::ParseError<&'a str>, F>(
@@ -136,10 +200,24 @@ mod test {
 
     use super::*;
 
-    const SINGLE_CLIPPING: &str = "Flow (Mihaly Csikszentmihalyi)\r
+    const SINGLE_HIGHLIGHT: &str = "Flow (Mihaly Csikszentmihalyi)\r
 - Your Highlight at location 1213-1214 | Added on Sunday, 12 July 2015 17:36:17\r
 \r
 The reason it is possible to achieve such complete involvement in a flow experience is that goals are usually clear, and feedback immediate.\r
+==========\r
+";
+
+    const SINGLE_NOTE: &str = "Flow (Mihaly Csikszentmihalyi)\r
+- Your Note at location 1213 | Added on Sunday, 12 July 2015 17:36:17\r
+\r
+Yada yada ya.\r
+==========\r
+";
+
+    const SINGLE_BOOKMARK: &str = "Sapiens: A Brief History of Humankind (Harari, Yuval Noah)\r
+- Your Bookmark at location 3883 | Added on Sunday, 22 October 2017 23:09:48\r
+\r
+\r
 ==========\r
 ";
 
@@ -165,16 +243,15 @@ The reason it is possible to achieve such complete involvement in a flow experie
     }
 
     #[test]
-    fn location() {
-        let res = parse_location(
-            "- Your Highlight at location 1213-1214 | Added on Sunday, 12 July 2015 17:36:17\r\n",
-        );
+    fn location_range() {
+        let res = parse_location("at location 1213-1214");
 
         assert_eq!(
             res,
             Ok((
                 "",
                 Location {
+                    kind: LocationKind::Location,
                     from: 1213,
                     to: Some(1214)
                 }
@@ -183,16 +260,15 @@ The reason it is possible to achieve such complete involvement in a flow experie
     }
 
     #[test]
-    fn note_location() {
-        let res = parse_note_location(
-            "- Your Note at location 1213 | Added on Sunday, 12 July 2015 17:36:17\r\n",
-        );
+    fn location_single() {
+        let res = parse_location("at location 1213");
 
         assert_eq!(
             res,
             Ok((
                 "",
                 Location {
+                    kind: LocationKind::Location,
                     from: 1213,
                     to: None
                 }
@@ -201,22 +277,46 @@ The reason it is possible to achieve such complete involvement in a flow experie
     }
 
     #[test]
-    fn text() {
-        let res = parse_text("Foo bar baz.\r\n==========\r\n");
+    fn location_page() {
+        let res = parse_location("on page 1213-1214");
 
-        assert_eq!(res, Ok(("", "Foo bar baz.".into(),)));
+        assert_eq!(
+            res,
+            Ok((
+                "",
+                Location {
+                    kind: LocationKind::Page,
+                    from: 1213,
+                    to: Some(1214)
+                }
+            ))
+        );
     }
 
     #[test]
     fn text_utf8() {
-        let res = parse_text("’Foo bar baz’.\r\n==========\r\n");
+        let res: nom::IResult<&str, &str> = parse_until(tag("xxx"))("’Foo bar baz’.xxx");
 
         assert_eq!(res, Ok(("", "’Foo bar baz’.".into(),)));
     }
 
     #[test]
     fn parse_single_clipping() {
-        let res = parse_clipping(SINGLE_CLIPPING);
+        let res = parse_clipping(SINGLE_HIGHLIGHT);
+
+        assert_debug_snapshot!(res);
+    }
+
+    #[test]
+    fn parse_single_note() {
+        let res = parse_clipping(SINGLE_NOTE);
+
+        assert_debug_snapshot!(res);
+    }
+
+    #[test]
+    fn parse_single_bookmark() {
+        let res = parse_clipping(SINGLE_BOOKMARK);
 
         assert_debug_snapshot!(res);
     }
